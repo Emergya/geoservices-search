2dbb3c899cb619e54c8fc969e3deb414c6197c75
adrianagm <adriana-4@hotmail.com>
2014-08-29 11:32:29 +0200

    Merge for Upstream

--- geoservices-search-roads/src/main/java/com/emergya/geoservices/search/roads/RoadSearcherImpl.java
+++ geoservices-search-roads/src/main/java/com/emergya/geoservices/search/roads/RoadSearcherImpl.java
@@ -28,19 +28,22 @@ public class RoadSearcherImpl implements RoadSearcher {
 
     @Autowired
     private PkDao pkSearchDAO;
+    
+    private final static String MISSING_NAME = "<missing Name>";
 
     @Override
-    public RoadResponse searchRoad(String query) {
+    public RoadResponse searchRoad(final String query) {
     	
     	final AtomicReference<String> wayCode = new AtomicReference<>();
     	final AtomicReference<String> km = new AtomicReference<>();
+    	
     	
     	PKLexer l = new PKLexer(new ANTLRInputStream(query));
     	PKParser p = new PKParser(new CommonTokenStream(l));
 	    p.addErrorListener(new BaseErrorListener() {
 	        @Override
 	        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-	            throw new IllegalStateException("failed to parse at line " + line + " due to " + msg, e);
+	           // Do nothing
 	        }
 	    });
 	    p.addParseListener(new PKBaseListener(){
@@ -56,7 +59,10 @@ public class RoadSearcherImpl implements RoadSearcher {
 	    p.pk();
 	    
 	    String wayname = wayCode.get();
-	    Integer pk_km = new Integer(0);
+	    if(MISSING_NAME.equals(wayname)){
+	    	wayname = query;
+	    }
+	    Integer pk_km = 0;
 	    if(km.get() != null){
 	    	pk_km = Integer.parseInt(km.get());
 	    }
