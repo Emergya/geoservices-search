044c7085d52d1143d8e3b101c9a2b9253ffdd162
Moises Arcos <marcos@emergya.com>
2014-09-30 13:33:41 +0200

    [#109781] Changes to add roads database as a config parameter

--- geoservices-search-roads/src/main/java/com/emergya/geoservices/search/roads/dao/RoadDaoImpl.java
+++ geoservices-search-roads/src/main/java/com/emergya/geoservices/search/roads/dao/RoadDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
@@ -17,13 +18,15 @@ import com.emergya.geoservices.search.roads.dto.Road;
 public class RoadDaoImpl extends JdbcBaseDao implements RoadDao {
 	
 	private static final RoadDataMapper MAPPER = new RoadDataMapper();
+	@Value("${geoservices.search.roads.roadsdatabase}")
+    private String ROADS_DATA_BASE;
 	
 	// TODO Prepare the request query
-	private static final String QUERY= "SELECT objectid, nombre FROM vial_li_com WHERE lower(nombre) like lower('%1$s')";
+	private static final String QUERY= "SELECT objectid, nombre FROM %1$s WHERE lower(nombre) like lower('%2$s')";
 
 	@Override
 	public List<Road> searchRoad(String roadName) {
-		String query = String.format(QUERY, "%"+ roadName.trim()+"%");
+		String query = String.format(QUERY, ROADS_DATA_BASE, "%"+ roadName.trim()+"%");
 		
 		List<Road> result = this.getTemplate().query(query, MAPPER);
         
